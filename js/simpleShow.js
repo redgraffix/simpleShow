e2d0453b783fc2ed0332b79146a4c39e355efb57
Jason Rodgers <red@redgraffix.com>
2013-03-09 13:18:22 -0800

    Update simpleShow.js

--- js/simpleShow.js
+++ js/simpleShow.js
@@ -10,13 +10,16 @@ function simpleShow(){
 	function simpleShowbgCss(){
 		$("#simpleShow , #sSCenter").css({'height': + $(window).height() + 'px','width': + $(window).width() + 'px'});};
 	function imgCss(){
-		$("#simpleShow img").css({'max-height': + ($(window).height()-30) + 'px','max-width': + ($(window).width()-30) + 'px'});};
+		$("#simpleShow img").css({'max-height': + ($(window).height()-80) + 'px','max-width': + ($(window).width()-40) + 'px'});
+		$("#simpleInfo").delay(200).animate({'top':'0px'},200);
+		};
 	//if the window sizechanges change the img size
 	$(window).resize(function(){simpleShowbgCss();imgCss();});
 	//start everyhting with a click				
 	$("a.viewThumb").click(function(){
-		//grab the link location
+		//grab the link and title
 		href = $(this).attr("href");
+		title = $(this).attr("title");
 		//hide the scrollbar
 		$("body").css({'overflow': 'hidden'});
 		//add the html to the doc
@@ -24,7 +27,7 @@ function simpleShow(){
 		//style the outer container	
 		simpleShowbgCss();
 		//make it visible, insert the img location, style it
-		$("#simpleShow").fadeIn(100, function(){$("#sSCenter").html("<img src='"+href+"'></div><div id='simpleClose'>x</div>");
+		$("#simpleShow").fadeIn(100, function(){$("#sSCenter").html("<div id='simpleInfo'>"+title+"<div id='simpleClose'>x</div></div><img src='"+href+"'>");
 		imgCss();
 	});
 	//make it go away	
