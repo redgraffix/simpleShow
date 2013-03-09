2c9937fc8eb1dc0a9794db138187efae9fd851d2
Jason Rodgers <red@redgraffix.com>
2013-03-09 12:01:15 -0800

    added html, js, css, and images

---
+++ js/simpleShow.js
@@ -0,0 +1,42 @@
+/*
+ *  simpleShow
+ *  Copyright (c) 2013 Jason Rodgers
+ *  very simple jquery lightbox plugin 
+ *  Licensed under the MIT license
+ *
+ */	
+function simpleShow(){	
+	//set up the sizing on the img and container
+	function simpleShowbgCss(){
+		$("#simpleShow , #sSCenter").css({'height': + $(window).height() + 'px','width': + $(window).width() + 'px'});};
+	function imgCss(){
+		$("#simpleShow img").css({'max-height': + ($(window).height()-30) + 'px','max-width': + ($(window).width()-30) + 'px'});};
+	//if the window sizechanges change the img size
+	$(window).resize(function(){simpleShowbgCss();imgCss();});
+	//start everyhting with a click				
+	$("a.viewThumb").click(function(){
+		//grab the link location
+		href = $(this).attr("href");
+		//hide the scrollbar
+		$("body").css({'overflow': 'hidden'});
+		//add the html to the doc
+		$("body").prepend("<div id='simpleShow'><div id='sSCenter'><img src='images/loading.gif'></div></div>");
+		//style the outer container	
+		simpleShowbgCss();
+		//make it visible, insert the img location, style it
+		$("#simpleShow").fadeIn(100, function(){$("#sSCenter").html("<img src='"+href+"'></div><div id='simpleClose'>x</div>");
+		imgCss();
+	});
+	//make it go away	
+	$("#simpleShow").click(function(){
+		//visibly hide it
+		$("#simpleShow").fadeOut(100);
+		//remove the actual html
+		setTimeout(function() {$("#simpleShow").remove();}, 105);
+		//put the scroll bar back
+		$("body").delay(105).css({'overflow': 'auto'});
+	});
+	return false;
+	});
+};
+	$(simpleShow);	
